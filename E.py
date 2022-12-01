81e30cfb7a28c1e55de3906a3631285d3cc1a36d
Ruben Van Landuyt <s142307@ap.be>
2022-12-01 14:56:10 +0100

    E bijna klaar

---
+++ E.py
@@ -0,0 +1,28 @@
+import turtle
+
+scrn = turtle.Screen()
+E = turtle.Turtle()
+E.pencolor('lime')
+E.pensize(3)
+E.pencolor('lime')
+E.penup()
+E.backward(50)
+E.pendown()
+
+E.left(90)
+E.forward(120)
+E.right(90)
+E.right(0)
+E.forward(80)
+E.penup()
+E.goto(-50,60)
+E.pendown()
+E.forward(50)
+E.penup()
+E.goto(-50,0)
+E.pendown()
+E.forward(90)
+
+
+
+turtle.exitonclick()
