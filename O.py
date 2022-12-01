d9410514cd79801ba498dea8cfad275678233078
Nando <nando.coomans@ap.be>
2022-12-01 14:12:15 +0100

    Niks

--- O.py
+++ O.py
@@ -1,10 +1,11 @@
-import turtle as t # load the turtle module
-
-t.penup()
-t.setpos(10,50)
-t.pendown()
-
-t.pensize(10)
-t.color("black")
-
-t.circle(100)
+import turtle # load the turtle module
+def drawnO():
+    t=turtle.Turtle()
+    t.penup()
+    t.setpos(10,50)
+    t.pendown()
+    t.pensize(10)
+    t.color("black")
+    t.circle(100)
+    t.exitonclick()
+drawnO()
