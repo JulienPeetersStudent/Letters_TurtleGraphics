0bd6b089aacd2798e0b5661001ebf982d51f49e4
Mohamed el hajioui <mohamed.elhajjioui@student.ap.be>
2022-12-01 13:55:26 +0100

    Add files via upload

---
+++ M.py
@@ -0,0 +1,15 @@
+import turtle
+
+scrn = turtle.Screen()
+pen = turtle.Turtle()
+
+pen.left(90)
+pen.forward(120)
+pen.right(135)
+pen.forward(70)
+pen.left(90)
+pen.forward(70)
+pen.right(135)
+pen.forward(120)
+
+turtle.done()
