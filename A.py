5ca1d0caa09e95b39d4c80f414c0cf717335d42a
Ahmet Kurtoglu <s143341@ap.be>
2022-12-08 14:54:37 +0100

    A

---
+++ A.py
@@ -0,0 +1,21 @@
+import turtle as t 
+
+scrn = t.Screen() 
+
+
+t.pensize(10)
+t.pencolor("black")
+t.penup()
+t.goto(50,100)
+t.pendown()
+t.right(60)
+t.forward(200)
+t.backward(200)
+t.left(-60)
+t.forward(200)
+t.backward(85)
+t.left(120)
+t.forward(115)
+t.right(60)
+t.forward(86)
+
